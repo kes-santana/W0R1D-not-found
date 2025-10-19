4de39c68e066ca1a1d0a882d9abd218e2694a567
DavidCastillo <davidcastillorodriguez2005@gmail.com>
2025-10-19 01:09:50 -0400

    Add Vector2D.

---
+++ where-worlds-fade/Scripts/Vector2D.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace WhereWorldsFade.Scripts
+{
+    public struct Vector2D(float x, float y)
+    {
+        public float x = x;
+        public float y = y;
+
+        public override string ToString() => $"({x}, {y})";
+
+        public static Vector2D operator +(Vector2D operand) => operand;
+        public static Vector2D operator -(Vector2D operand)
+            => new(-operand.x, -operand.y);
+        public static Vector2D operator +(Vector2D left, Vector2D right)
+            => new(left.x + right.x, left.y + right.y);
+        public static Vector2D operator -(Vector2D left, Vector2D right)
+            => new(left.x - right.x, left.y - right.y);
+        public static Vector2D operator *(Vector2D left, Vector2D right)
+            => new(left.x * right.x, left.y * right.y);
+        public static Vector2D operator /(Vector2D left, Vector2D right)
+        {
+            if (right.x == 0 || right.y == 0)
+                throw new DivideByZeroException();
+            return new(left.x / right.x, left.y / right.y);
+        }
+        public static float Dot(Vector2D left, Vector2D right) 
+            => left.x * right.x + left.y * right.y;
+    }
+}
